08ae5f7077330fa21961c5c997e25c8f37ea96f5
= <jdhaimson@gmail.com>
2013-12-11 13:56:12 -0500

    Test ClientSendProtocol

--- src/client/ClientSendProtocol.java
+++ src/client/ClientSendProtocol.java
@@ -29,9 +29,7 @@ public class ClientSendProtocol implements Runnable {
      */
     @Override
     public void run() {
-        System.out.println("Make Request: "+message);
 		out.println(message);
-		
     }
 
 }
